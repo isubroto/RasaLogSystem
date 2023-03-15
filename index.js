daf1ff00c6954efccdcc67de85e8d2bc6293aa12
Subroto Saha <22-92432-1@student.aiub.edu>
2023-03-15 09:27:33 +0600

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+import log from './lib/helpers/rasabot.js';
+rasa.baseUrl('http://128.199.201.218:5500');
+rasa.parseModel("hi",res=>{
+  console.log(res);
+})
+/*deletedata()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());*/
+
+/*getdata()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());*/
