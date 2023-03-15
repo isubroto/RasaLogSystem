c14d9becf492f7bf998530c62c4ebcdfb8fb48f8
Subroto Saha <22-92432-1@student.aiub.edu>
2023-03-15 14:08:16 +0600

    initial release 0.0.1

--- index.js
+++ index.js
@@ -9,15 +9,4 @@ rasa_log.CreateLog=log.create;
 rasa_log.GetLog=log.Find;
 rasa_log.Export=log.export;
 
-
-
-log.botUrl('http://128.199.201.218:5500');
-log.databaseUrl("mongodb://root:password@localhost:27017");
-log.create("hi",res=>{
-  log.Insert(res);
-});
-let data= await log.Find();
-console.log(data);
-//log.export(data);
-
 export default rasa_log;
